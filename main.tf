5d7a87a8d186f3089e86d257435c4d602e544c6c
Sandeep <78046533+SandeepDas01@users.noreply.github.com>
2022-03-24 00:33:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAW7ZBD4KD2TDDPSUI"
+  secret_key = "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"
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-04893cdb768d0f9ee"
+  instance_type = "t2.micro"
+
+
